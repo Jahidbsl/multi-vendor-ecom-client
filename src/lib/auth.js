d27d077c10246a49dc7c5fd4faadbf112b7d77bf
jahid <jahidhasanbsl46@gmail.com>
2026-07-28 16:13:52 +0600

    use google login

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -26,4 +26,9 @@ export const auth = betterAuth({
     emailAndPassword: { 
     enabled: true, 
   }, 
+   socialProviders: {
+        google: { 
+            clientId: process.env.GOOGLE_CLIENT_ID, 
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET, 
+        }, 
 });
